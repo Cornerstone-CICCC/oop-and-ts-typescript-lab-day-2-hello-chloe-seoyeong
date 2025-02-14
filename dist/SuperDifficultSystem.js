e5b9f5a9b205243bad04d6f8d96fefb7479d3840
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-14 14:00:38 -0800

    Modified SuperDifficultSystem

--- dist/SuperDifficultSystem.js
+++ dist/SuperDifficultSystem.js
@@ -1,27 +1,21 @@
-//
-// 🤖 Build a Super Difficult System
-// 
-//
-// 
-//
-// 
-//
-// 
-//
-// 
-//
-// Just kidding.
-//
-//
-// Happy Valentines Day to you! 💖
-// Thank you for being my student! 😜
-// Good luck with the rest of the lab,
-// ...and I hope you enjoy your day!
-//
-//
-//
-//////////////////////////////
-//
-// Elemr,
-// Thank you for your lecture and Valentines gift like this file. LOL
-// Enjoy your rest of day and Have a great weekend!
+class SayGreeting {
+    guest;
+    message = [];
+    constructor(guest) {
+        this.guest = guest;
+    }
+    add(msg) {
+        this.message.push(msg);
+    }
+    combine() {
+        let longMsg = "";
+        this.message.forEach(msg => longMsg += `\n${msg}`);
+        return longMsg;
+    }
+}
+const guest = new SayGreeting("Elmer");
+guest.add("Happy Valentine! 💝");
+guest.add("Thank you for lecture.");
+guest.add("And have a wonderful long weekend.");
+guest.add("See you next Tuesday! 👋");
+console.log(guest.combine());
--- src/SuperDifficultSystem.ts
+++ src/SuperDifficultSystem.ts
@@ -25,3 +25,31 @@
 // Elemr,
 // Thank you for your lecture and Valentines gift like this file. LOL
 // Enjoy your rest of day and Have a great weekend!
+type Message = {
+  msg: string
+}
+
+class SayGreeting {
+  guest: string;
+  message: Message[] = [];
+  constructor(guest: string) {
+    this.guest = guest;
+  }
+
+  add(msg) {
+    this.message.push(msg);
+  }
+
+  combine(): string{
+    let longMsg: string = "";
+    this.message.forEach(msg => longMsg += `\n${msg}`)
+    return longMsg;
+  }
+}
+
+const guest = new SayGreeting("Elmer");
+guest.add("Happy Valentine! 💝")
+guest.add("Thank you for lecture.")
+guest.add("And have a wonderful long weekend.")
+guest.add("See you next Tuesday! 👋")
+console.log(guest.combine())
